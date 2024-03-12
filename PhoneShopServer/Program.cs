f2699a510163bba0b4eb48c1514bebae1576537c
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-03-12 12:36:12 +1030

    Microsoft SQL DB Connection and integration with first model

--- PhoneShopServer/Program.cs
+++ PhoneShopServer/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PhoneShopServer.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,16 @@ builder.Services.AddRazorPages();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//starting
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")?? throw new InvalidOperationException("Connection string 'Default' not found."));
+
+});
+// Ending...
+
+
 
 var app = builder.Build();
 
