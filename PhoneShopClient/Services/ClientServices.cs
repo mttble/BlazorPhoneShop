c453176c452c1677395d78c071d9f40059fb521e
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-03-14 09:00:21 +1030

    added MessageDialog component and implemented in HandleSaveProduct

---
+++ PhoneShopClient/Services/ClientServices.cs
@@ -0,0 +1,18 @@
+﻿using PhoneShopSharedLibrary.Contracts;
+using PhoneShopSharedLibrary.Models;
+using PhoneShopSharedLibrary.Responses;
+
+namespace PhoneShopClient.Services;
+
+public class ClientServices : IProduct
+{
+    public Task<ServiceResponse> AddProduct(Product model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Product>> GetAllProducts(bool featuredProducts)
+    {
+        throw new NotImplementedException();
+    }
+}
